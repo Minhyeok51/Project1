e464434dcb659a88792e1d67857c1f62fd4c4484
minhyeok <minhyeok51@naver.com>
2022-09-19 17:30:30 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -14,12 +14,25 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="black">
+    <div>
+      <Navbar bg="none" >
         <Container>
-          <Navbar.Brand href="/" id="logo"><h2>Movie</h2></Navbar.Brand>
+          <Navbar.Brand href="/" id="logo">
+            MOVIE
+          </Navbar.Brand>
         </Container>
-      </Navbar>
+        <img src="img/background.jpg" className="background"></img>
+      </Navbar><br/><br/><br/><br/><br/><br/><br/>
+      <div className="section" >
+        
+        <h1>반갑습니다.</h1>
+        <h2>
+          영화와 시리즈를 <br />
+          무제한으로.
+        </h2>
+        <h3>다양한 디바이스에서 시청하세요. 언제든 해지하실 수 있습니다.</h3>
+        <button id="start">시작하기 ></button>
+      </div>
     </div>
   );
 }
